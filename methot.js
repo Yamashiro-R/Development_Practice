bdeef3b414dfa63a950b1be39acbce2cbb0daa30
yamashiro-R <yamashiro177@gmail.com>
2022-11-16 13:02:59 +0900

    詳細欄追加！まだ完全ではないが皆で編集するために

--- methot.js
+++ methot.js
@@ -6,17 +6,19 @@ var l1 = document.getElementById('text_info');
 var fen = document.getElementsByClassName('first-exam_test');
 
 let first_exam = document.querySelectorAll(`input[type='checkbox'][name='First-stage exam']`);
-
+/*
 for (let i = 0; i < first_exam.length;i++) {
     exam_detail(first_exam,i);
-}
+}*/
 
-
+//Input_2_2～3のチェックボタンに応じてtextareaが出力される処理。
+/*
 function exam_detail(first_exam,i){
 
     first_exam[i].addEventListener('click',function(){
         if (this.checked) {
             l1.innerHTML += '<div id="new'+ i +'" class="testsAll"><p class="title-tests">' + fen[i].textContent + '内容詳細：</p><textarea  class="text-tests" name="details"></textarea></div>';
+            
             check[i] = document.getElementById(ii).textContent;
 
         } else {
@@ -34,12 +36,133 @@ function exam_detail(first_exam,i){
                 }
             }
         }},false);
+}*/
+/*
+function exam_check(cnt){
+    const first_exam = document.querySelectorAll(`input[type='checkbox'][name='First-stage exam']`);
+    const div_class = document.getElementsByClassName('first-exam_test');
+    //消去注意文
+    const result = "記入内容はすべて削除されます。よろしいですか？";
+    
+    let exam_type;
+    let place;
+    let div = document.createElement('div');
+    let para = document.createElement('p');
+    let area = document.createElement('textarea');
+
+    constructor(cnt){
+        this.exam_type = let + cnt;
+        this.place = first_exam[cnt];
+    }
+
+    
+    place.addEventListener('input',()=>{
+        if(hikki1.checked){
+            hikki1_div.appendChild(hikki1_para);
+            hikki1_div.appendChild(hikki1_area);
+            l1.appendChild(hikki1_div); 
+        }else{
+            console.log(hikki1_area.value);
+            //textareaに入力が無い時
+            if( hikki1_area.value == ""){
+                hikki1_div.remove();
+            }else{
+                //textareaに入力が有る時
+                window.confirm(result);
+                hikki1_div.remove();
+            }
+        }
+    });
+*/
+
+}
+
+function exam_detail_2(){
+    let div_class = document.getElementsByClassName('first-exam_test');
+
+    //筆記(専門)
+    let hikki1 = first_exam[0];
+    //筆記(一般)
+    let hikki2 = first_exam[1];
+    //適性(専門)
+    let tekisei1 = first_exam[2];
+    //適性(一般)
+    let tekisei2 = first_exam[3];
+
+    
+
+
+    for(let i=0;i<2;i++){
+        
+    }
+
+
+    
+    //div,p,textareaをクリエイト
+    let hikki1_div = document.createElement('div');
+    let hikki1_para = document.createElement('p');
+    let hikki1_area = document.createElement('textarea');
+    
+    let hikki2_div = document.createElement('div');
+    let hikki2_para = document.createElement('p');
+    let hikki2_area = document.createElement('textarea');
+
+
+   
+
+    hikki1_area.id = "test";
+    hikki1_para.textContent = div_class[0].textContent +"詳細内容";
+
+    //消去注意文
+    let result = "記入内容はすべて削除されます。よろしいですか？";
+
+    //筆記(専門)
+    hikki1.addEventListener('input',()=>{
+        if(hikki1.checked){
+            hikki1_div.appendChild(hikki1_para);
+            hikki1_div.appendChild(hikki1_area);
+            l1.appendChild(hikki1_div); 
+        }else{
+            console.log(hikki1_area.value);
+            //textareaに入力が無い時
+            if( hikki1_area.value == ""){
+                hikki1_div.remove();
+            }else{
+                //textareaに入力が有る時
+                window.confirm(result);
+                console.log(hikki1_div);
+                hikki1_div.remove();
+                hikki1_area.value = "";
+                
+            }
+        }
+    });
+
+    //筆記(一般)
+    hikki2.addEventListener('input',()=>{
+        if(hikki2.checked){
+            hikki2_div.appendChild(hikki2_para);
+            hikki2_div.appendChild(hikki2_area);
+            console.log(hikki2_div);
+            l1.appendChild(hikki2_div); 
+        }else{
+            console.log(hikki2_area.value);
+            if( hikki2_area.value == ""){
+                hikki2_div.remove();
+            }else{
+                window.confirm(result);
+                hikki2_div.remove();
+            }
+        }
+    });
+
+
+
 }
 
 
 function new_login_check() {
         let formElements = document.forms[0];
-        
         for (let con = 0; con < 4; con++) {
             if (formElements.elements[con].value != "" )  {
                 if (con == 3) {
@@ -52,13 +175,25 @@ function new_login_check() {
                 break;
             }
         }
+        if (formElements.elements[1].value == formElements.elements[2].value) {
+            ;
+        } else {
+            alert("パスワードが間違っています。");
+            formElements.elements[1].value = null;
+            formElements.elements[2].value = null;
+        }
+}
+      
+
+/*
+バリデーションチェックが完成次第消す予定
 
 function Input_Form_1_check() {
     let formElements = document.forms[0];
     let flag = false;
     let firstflag = true;
-    let arraycheck = Array(7);  /*チェックぼっくボックスのboolen値を格納する配列*/
-    let i = 0; /*チェックボックスの添え字*/
+    let arraycheck = Array(7);  //チェックぼっくボックスのboolen値を格納する配列
+    let i = 0; //チェックボックスの添え字
     
     let checkfalg = false;
     
@@ -80,7 +215,7 @@ function Input_Form_1_check() {
                 }
                 break;
 
-            case 3:     /*書類の有無チェック*/
+            case 3:     //書類の有無チェック
             case 4:
                 if(formElements.elements[cnt].checked){
                     flag = true;   
@@ -95,13 +230,13 @@ function Input_Form_1_check() {
                 break;
             
             default:
-                /*どれか一つでもtrueならok*/
+                //どれか一つでもtrueならok
                 if(formElements.elements[cnt].checked){
                     checkfalg = true;
                 }       
         }
     }
-    /*チェックボックス欄に一つでもチェックが入っているかの判定*/
+    //チェックボックス欄に一つでもチェックが入っているかの判定
     if(checkfalg){
         ;
     }else{
@@ -118,26 +253,240 @@ function Input_Form_1_check() {
     
 
 }
-    
-/*
-function HelloPHP(){
-    jQuery.ajax({
-
-        type: 'post',
-        url: 'Input_Form_1.php',
-        dataType: 'json',
-        data:{
-
-        }
-        .done(function(response){
-
-        })
-        .fail(function(xhr) {
-            alert("サーバーとの通信に失敗しました。");
-        })
-    }); 
-}
 */
+
+//Input_Form_2_1バリデーションチェック用↓
+const Allprefecture = ["沖縄県","北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県",
+"千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県",
+"静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県",
+"岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県",
+"大分県","宮崎県","鹿児島県"];
+
+    
+function validation_check(){
+    let formElements = document.forms[0];
+    let company = formElements.elements[0];
+    let address = formElements.elements[1];
+    let methot = formElements.elements[2];
+    let document_radio = document.getElementById("radio");
+    let job = formElements.elements[5];
+    let number = formElements.elements[6]; 
+    let documents_checkbox = document.getElementById("documents_checkbox");
+    let savebtn = formElements.elements[16]; 
+    let submitbtn = formElements.elements[17]; 
+    
+    //エラー出力先を配列で取得
+    let denger = formElements.getElementsByClassName('denger_field');
+    console.log(denger);
+    
+    //其々の入力が正常化判断する為の boolean値を格納する為の変数
+    let activecompany;
+    let activeaddress;
+    let activemethot;
+    let activedocument_radio;
+    let activejob;
+    let activenumber;
+    let activedocuments_checkbox;
+    
+    //其々のパラグラフをcreate!!この中にエラー文の文章を格納する。
+    let companypara = document.createElement('p');
+    let addresspara = document.createElement('p');
+    let methotpara = document.createElement('p');
+    let jobpara = document.createElement('p');
+    let numberpara = document.createElement('p');
+
+    //探索用のカウント
+    let cnt = 0;
+    //入力値の県名を格納
+    let prefecture;
+
+
+    //保存ボタンをデフォルトで無効化
+    savebtn.disabled = true; 
+    //一次→ボタンをデフォルトで無効化
+    submitbtn.disabled = true;
+
+    //保存ボタンと一次→ボタンの有効化条件
+    formElements.addEventListener('input',()=>{
+        if(activecompany == true && activeaddress == true && activemethot == true &&
+            activedocument_radio == true && activejob && activenumber == true &&
+            activedocuments_checkbox){
+                savebtn.disabled = false;
+                submitbtn.disabled = false;
+            }else{
+                savebtn.disabled = true;
+                submitbtn.disabled = true;
+            }
+    });
+
+    formElements.addEventListener('change',()=>{
+        if(activecompany == true && activeaddress == true && activemethot == true &&
+            activedocument_radio == true && activejob && activenumber == true &&
+            activedocuments_checkbox){
+                savebtn.disabled = false;
+                submitbtn.disabled = false;
+            }else{
+                savebtn.disabled = true;
+                submitbtn.disabled = true;
+            }
+    });    
+    //企業名入力欄
+    company.addEventListener("input",()=>{
+        if(company.value === ""){   //入力値がない時
+            //入力フォームが空の場合
+            companypara.textContent = "表示名を入力してください";
+            companypara.id = "row_1_para";
+            denger[0].appendChild(companypara);
+            activecompany = false;
+        }else if(company.value.length < 3 ){    //入力値が3文字以下の時
+            companypara.textContent = "入力値が違います。";
+            companypara.id = "row_1_para";
+            denger[0].appendChild(companypara);
+            activecompany = false;
+        }else if(company.value.length > 30){    //入力値が30文字以上の時
+            companypara.textContent = "入力値が多すぎます。";
+            companypara.id = "row_1_para";
+            denger[0].appendChild(companypara);
+            activecompany = false;
+        }else{  //バリデーションチェックOKの時
+            companypara.textContent = "";
+            companypara.id = "row_1_para";
+            denger[0].appendChild(companypara);
+            activecompany = true;   
+        }
+    });
+    //企業の住所入力欄
+    address.addEventListener("input",()=>{
+        if(address.value === ""){
+            addresspara.textContent = "応募企業の住所を入力してください。";
+            addresspara.id = "row_1_para";
+            denger[1].appendChild(addresspara);
+            activeaddress = false;
+        }else if( address.value[2] == "県" || address.value[3] == "県" ||
+                    address.value[2] == "都" || address.value[2] == "府" || address.value[2] == "道" ){
+            if( address.value.search("県") != -1 ){ //県が含まれている。
+                switch( address.value.search("県") ){
+                    case 2: //3番目に県がくるる時(沖縄県等)
+                        prefecture = address.value.substr(0,3);
+                        break;
+                    case 3: //4番目に県がくる時(神奈川県等)
+                        prefecture = address.value.substr(0,4);
+                    }
+            }else{ prefecture = address.value.substr(0,3); } //"東京都、大阪府、北海道" 
+            
+            //一致する都道府県を探索＆安策する前に初期化
+            cnt = 0; 
+            for(let tmp in Allprefecture) {
+                if(prefecture != Allprefecture[tmp]){ 
+                    cnt++;      
+                }else{
+                    break;
+                }
+            }
+
+            console.log("カウントの値：" + cnt);
+
+            if(cnt < 47) {  //都道府県名で探索成功の時
+                addresspara.textContent = "";
+                addresspara.id = "row_1_para";
+                denger[1].appendChild(addresspara);
+                activeaddress = true;
+                console.log("成功");
+            } 
+        }else{
+            addresspara.textContent = "県名/市町村の順に入力してください。"
+            addresspara.id = "row_1_para";
+            denger[1].appendChild(addresspara);
+            activeaddress = false;
+        }
+        
+
+    });
+    //応募方法入力欄
+    methot.addEventListener("input",()=>{
+        switch (methot.value){
+            case "0":  //未選択の時
+                console.log("テスト");
+                methotpara.textContent = "応募方法を選択してください。";
+                methotpara.id = "row_1_para";
+                denger[2].appendChild(methotpara);
+                activemethot = false;
+                break;
+            default:
+                methotpara.textContent = "";
+                methotpara.id = "row_1_para";
+                denger[2].appendChild(methotpara);
+                activemethot = true;
+        }
+        
+    });
+    //書類選考の有無チェック欄
+    document_radio.addEventListener("change",()=>{
+        console.log("変更ok");
+    });
+
+    //職種入力欄
+    job.addEventListener("input",()=>{
+        if(job.value === ""){   
+            //入力値がない時
+            jobpara.textContent = "表示名を入力してください";
+            jobpara.id = "row_1_para";
+            denger[4].appendChild(jobpara);
+            activejob = false;
+        }else{
+            jobpara.textContent = "";
+            jobpara.id = "row_1_para";
+            denger[4].appendChild(jobpara);
+            activejob = true;
+        }
+
+    });
+    //応募件数入力欄
+    number.addEventListener("input",()=>{
+        if(number.value === ""){   
+            //入力値がない時
+            numberpara.textContent = "件数を入力してください。";
+            numberpara.id = "row_1_para";
+            denger[5].appendChild(numberpara);
+            activenumber = false;
+        }else if(isNaN(number.value)){ 
+            //非数であればtrue
+            numberpara.textContent = "数字を入力してください。";
+            numberpara.id = "row_1_para";
+            denger[5].appendChild(numberpara);
+            activenumber = false;
+        }else if(number.value < 1){
+            //マイナス値が入力されている
+            numberpara.textContent = "有効な数字を入力してください。";
+            numberpara.id = "row_1_para";
+            denger[5].appendChild(numberpara);
+            activenumber = false;
+        }else if(number.value > 10){
+            //10件以上の時
+            console.log("テスト");
+            numberpara.textContent = "10件以上はエラーとみなす。";
+            numberpara.id = "row_1_para";
+            denger[5].appendChild(numberpara);
+            activenumber = false;
+        }else{
+            numberpara.textContent = "";
+            numberpara.id = "row_1_para";
+            denger[5].appendChild(numberpara);
+            activenumber = true;
+        }
+    });
+    //提出書類チェック欄
+    documents_checkbox.addEventListener("change",()=>{
+        let div_sele = document.getElementsByClassName('docu_sele');
+            console.log(div_sele);
+    });
+
+
+
+
+    
+}
+
 
 function parseStrToBoolean(str){
     return (str == true) ? true : false;
@@ -146,39 +495,4 @@ function parseStrToBoolean(str){
 
 
 
-/*
 
-s2_1.addEventListener('click',function(){
-if (this.checked) {
-    l1.innerHTML = '<p class="p-info">筆記(専門)<br>内容詳細：</span></p><textarea id="once_starge_exam_detail" class=" Input_Form_2_1_input-view"></textarea>';
-} else {
-    l1.innerHTML = '';
-}},false);
-
-var l2 = document.getElementById('text_info2');
-s2_2.addEventListener('click',function(){
-    if (this.checked) {
-        l2.innerHTML = '筆記(一般常識)内容詳細：<textarea id="once_starge_exam_detail" class=" Input_Form_2_1_input-view test-a"></textarea>';
-    } else {
-        l2.innerHTML = '';
-    }},false);
-
-
-var l3 = document.getElementById('text_info3');
-s2_3.addEventListener('click',function(){
-    if (this.checked) {
-        l3.innerHTML = '適性検査(専門)内容詳細：<textarea id="once_starge_exam_detail" class=" Input_Form_2_1_input-view"></textarea>';
-    } else {
-        l3.innerHTML = '';
-    }},false);
-
-    
-        if (formElements.elements[1].value == formElements.elements[2].value) {
-            ;
-        } else {
-            alert("パスワードが間違っています。");
-            formElements.elements[1].value = null;
-            formElements.elements[2].value = null;
-        }
-
-}
