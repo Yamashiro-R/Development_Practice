1ad069c1be428280f2a8886b2dd2ed3042b49097
yamashiro-R <yamashiro177@gmail.com>
2022-11-18 17:18:06 +0900

    home画面改装中

--- home_2.php
+++ home_2.php
@@ -12,7 +12,9 @@
             <meta charset="UTF-8">
             <link rel="stylesheet" href="cssfiles/style.css">
             <link rel="stylesheet" href="cssfiles/style_prot.css">
-            
+            <!-- アイコン用のkit -->
+            <script src="https://kit.fontawesome.com/fd6da7ad7b.js" crossorigin="anonymous"></script>
+
             <title>ホーム画面</title>
         </head>
         <body class="home">
@@ -22,15 +24,21 @@
 
             <div class="prot_div">
                 <div class="new_title"> 
-                    <div>TOP</div>
-                    <div>報告書作成</div>
+                    <div class="title">TOP</div>
+                    <div class="title">報告書作成</div>
                 </div>
                 <div class="prot_body">
-                 
+                    <div>検索</div>
                 </div>
             </div>
+            
+            <script type="text/javascript" src="home.js"></script>
+            <script type="text/javascript">setting()</script>
 
         </body>
+
+
+
     
     </html>
 
